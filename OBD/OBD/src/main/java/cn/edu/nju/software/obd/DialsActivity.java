71eb1ed0c40821def8aa28a2e7ef2908c167827e
黄家敏 <hjmsens@gmail.com>
2014-03-19 17:16:19 +0800

    Add voltage dial Add text information Remove xml animations, use code instead

--- OBD/OBD/src/main/java/cn/edu/nju/software/obd/DialsActivity.java
+++ OBD/OBD/src/main/java/cn/edu/nju/software/obd/DialsActivity.java
@@ -5,13 +5,17 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
+import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
 
 /**
  * Shows the current status of the car
  */
 public class DialsActivity extends Activity {
+    private static final double VOLTAGE_MAX_ANGLE = 129.0;
+    private static final double SPEED_MAX_ANGLE = 246.33;
+
+    private ImageView mVoltageDialPointer;
     private ImageView mSpeedDialPointer;
 
     @Override
@@ -24,15 +28,31 @@ public class DialsActivity extends Activity {
             getActionBar().setDisplayHomeAsUpEnabled(true);
         }
 
+        mVoltageDialPointer = (ImageView) findViewById(R.id.voltage_dial_pointer);
         mSpeedDialPointer = (ImageView) findViewById(R.id.speed_dial_pointer);
+
+        mVoltageDialPointer.post(new Runnable() {
+            @Override
+            public void run() {
+                RotateAnimation rotateAnimation = new RotateAnimation(0.0f, 100.0f,
+                        Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 1.0f);
+                rotateAnimation.setInterpolator(DialsActivity.this, android.R.anim.accelerate_decelerate_interpolator);
+                rotateAnimation.setDuration(1000);
+                rotateAnimation.setFillEnabled(true);
+                rotateAnimation.setFillAfter(true);
+                mVoltageDialPointer.startAnimation(rotateAnimation);
+            }
+        });
         mSpeedDialPointer.post(new Runnable() {
             @Override
             public void run() {
-                Animation rotateAnimation = AnimationUtils.
-                        loadAnimation(DialsActivity.this, R.anim.pointer_rotate);
-                if (rotateAnimation != null) {
-                    mSpeedDialPointer.startAnimation(rotateAnimation);
-                }
+                RotateAnimation rotateAnimation = new RotateAnimation(0.0f, 100.0f,
+                        Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+                rotateAnimation.setInterpolator(DialsActivity.this, android.R.anim.accelerate_decelerate_interpolator);
+                rotateAnimation.setDuration(1000);
+                rotateAnimation.setFillEnabled(true);
+                rotateAnimation.setFillAfter(true);
+                mSpeedDialPointer.startAnimation(rotateAnimation);
             }
         });
     }
