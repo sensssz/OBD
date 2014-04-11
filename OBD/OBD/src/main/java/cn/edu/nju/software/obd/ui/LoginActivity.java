385f5e087404bee98c0bea42484566862a66064f
黄家敏 <hjmsens@gmail.com>
2014-04-11 11:16:11 +0800

    Modification of login logic

--- OBD/OBD/src/main/java/cn/edu/nju/software/obd/ui/LoginActivity.java
+++ OBD/OBD/src/main/java/cn/edu/nju/software/obd/ui/LoginActivity.java
@@ -2,27 +2,17 @@ package cn.edu.nju.software.obd.ui;
 
 import android.app.ActionBar;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import org.apache.http.NameValuePair;
-import org.apache.http.message.BasicNameValuePair;
-
-import java.io.IOException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.ArrayList;
-import java.util.List;
 
 import cn.edu.nju.software.obd.R;
-import cn.edu.nju.software.obd.network.HttpClient;
-import cn.edu.nju.software.obd.network.Url;
 import cn.jpush.android.api.InstrumentedActivity;
-import cn.jpush.android.api.JPushInterface;
 
 public class LoginActivity extends InstrumentedActivity {
     private EditText mUsernameEdit;
@@ -54,40 +44,43 @@ public class LoginActivity extends InstrumentedActivity {
                 } else if (password.length() < 1) {
                     showMessage(R.string.password_empty);
                 } else {
-                    new AsyncTask<Void, Void, String>() {
-                        @Override
-                        protected String doInBackground(Void... voids) {
-                            String MD5edUsername = MD5(username);
-                            String MD5edPassword = MD5(password);
-                            List<NameValuePair> parameters = new ArrayList<NameValuePair>(2);
-                            parameters.add(new BasicNameValuePair("username", MD5edUsername));
-                            parameters.add(new BasicNameValuePair("password", MD5edPassword));
-                            try {
-                                return HttpClient.getInstance().httpPost(Url.LOGIN_URL, parameters);
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
-                            return null;
-                        }
-
-                        @Override
-                        protected void onPostExecute(String signInResult) {
-                            if (signInResult == null) {
-                                showMessage(R.string.connection_fail);
-                                return;
-                            }
-                            String[] results = signInResult.split(",");
-                            if (results[0].equals("1")) {
-                                JPushInterface.setAlias(LoginActivity.this, results[1], null);
-                                showMessage(R.string.login_success);
-                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                                startActivity(intent);
-                                finish();
-                            } else {
-                                showMessage(R.string.login_fail);
-                            }
-                        }
-                    }.execute();
+                    showMessage(R.string.login_success);
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+//                    new AsyncTask<Void, Void, String>() {
+//                        @Override
+//                        protected String doInBackground(Void... voids) {
+//                            String MD5edUsername = MD5(username);
+//                            String MD5edPassword = MD5(password);
+//                            List<NameValuePair> parameters = new ArrayList<NameValuePair>(2);
+//                            parameters.add(new BasicNameValuePair("username", MD5edUsername));
+//                            parameters.add(new BasicNameValuePair("password", MD5edPassword));
+//                            try {
+//                                return HttpClient.getInstance().httpPost(Url.LOGIN_URL, parameters);
+//                            } catch (IOException e) {
+//                                e.printStackTrace();
+//                            }
+//                            return null;
+//                        }
+//
+//                        @Override
+//                        protected void onPostExecute(String signInResult) {
+//                            if (signInResult == null) {
+//                                showMessage(R.string.connection_fail);
+//                                return;
+//                            }
+//                            String[] results = signInResult.split(",");
+//                            if (results[0].equals("1")) {
+//                                JPushInterface.setAlias(LoginActivity.this, results[1], null);
+//                                showMessage(R.string.login_success);
+//                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+//                                startActivity(intent);
+//                                finish();
+//                            } else {
+//                                showMessage(R.string.login_fail);
+//                            }
+//                        }
+//                    }.execute();
                 }
             }
         });
